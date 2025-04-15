d65de457a40213b217a5ed6be74369cd403152fe
ketto26 <86974154+ketto26@users.noreply.github.com>
2025-04-15 18:31:56 +0400

    Add initial Xcode project

---
+++ PetTranslator/PetTranslator/PetTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetTranslatorApp.swift
+//  PetTranslator
+//
+//  Created by Keto Nioradze on 15.04.25.
+//
+
+import SwiftUI
+
+@main
+struct PetTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
